c1a9d70a075fdf5a055b13177261432d9bd476fa
yehia326 <yehia.elgeretly1@gmail.com>
2025-03-15 21:42:37 +0200

    an error has occured

--- my test.py
+++ my test.py
@@ -10,7 +10,7 @@ def determine_winner(player1_choice, player2_choice):
          (player1_choice == "scissors" and player2_choice == "paper"):
         return "Player 1 wins!"
     else:
-        return "Player 2 wins!"  #
+        return "Player 2 wins!  #
 
 def play_game (): # 
     player1_choice = get_player_choice(1)
